4d706f3749dd2eebd788c695fa6602b01dfd9006
Chanon Jenakom <chanonjenakom@gmail.com>
2018-05-09 13:13:43 +0700

    Update 'test_stationarity' function

--- mine_time_series_data.py
+++ mine_time_series_data.py
@@ -14,17 +14,17 @@ def set_window_title(window_title):
 
 # Test the stationarity of the time series.
 def test_stationarity(time_series, window_title):
-    # Create the statistics.
+    # Create the test statistics.
     dataframe = pd.DataFrame(time_series)
     rolling_mean = dataframe.rolling(window=12, center=False).mean()
     rolling_std = dataframe.rolling(window=12, center=False).std()
-
-    # Show the result of Dickey-Fuller Test.
-    print('Results of Dicky-Fuller Test:')
     df_test = adfuller(time_series, autolag='AIC')
     df_output = pd.Series(df_test[0:4], index=['Test Statistic', 'p-value', '#Lags Used', 'Number of Observations Used'])
     for key, value in df_test[4].items():
         df_output['Critical Value (%s)' % key] = value
+
+    # Show the result of Dickey-Fuller Test.
+    print('Results of Dicky-Fuller Test:')
     print(df_output)
 
     # Plot the statistics.
